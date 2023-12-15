eb9c389faa63bc3e0d2a649258c04470c302c57f
Prince <prince.agarwal@webware.io>
2023-12-15 11:01:00 +0530

    Added feature_1

--- app/src/main/java/com/example/gitproject/First.java
+++ app/src/main/java/com/example/gitproject/First.java
@@ -4,7 +4,7 @@ public class First {
 
     //Addded project in github.
 
-    //Added feature one branch in git hub
+    //Added feature one branch in git hub  -- (feature one modified in feature 1 branch)
 
     //Added feature two branch in git hub.
 }
